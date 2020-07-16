d18aa3b5988dd8d3b034d6293e8417c2038c88aa
zdazx <zhangduo0227@163.com>
2020-07-16 17:23:41 +0800

    floating Type Test

--- src/test/java/com/cultivation/javaBasic/FloatingTypeTest.java
+++ src/test/java/com/cultivation/javaBasic/FloatingTypeTest.java
@@ -15,7 +15,7 @@ class FloatingTypeTest {
 
         // TODO: Please change the result to pass the test.
         // <!--start
-        final int expected = Integer.MAX_VALUE;
+        final int expected = 2;
         // --end-->
 
         assertEquals(expected, integer);
@@ -41,7 +41,7 @@ class FloatingTypeTest {
 
         // TODO: Please change the result to pass the test.
         // <!--start
-        final int expected = Integer.MAX_VALUE;
+        final int expected = 2;
         // --end-->
 
         assertEquals(expected, integer);
@@ -54,7 +54,7 @@ class FloatingTypeTest {
 
         // TODO: Please call some method to round the floating point number.
         // <!--start
-        final long rounded = Long.MAX_VALUE;
+        final long rounded = (long) Math.ceil(floatingPointNumber);
         // --end-->
 
         assertEquals(3L, rounded);
@@ -63,12 +63,12 @@ class FloatingTypeTest {
     @SuppressWarnings("unused")
     private boolean isNan(double realNumber) {
         // TODO: please find the method that performs this operation.
-        throw new NotImplementedException();
+        return new Double(realNumber).isNaN();
     }
 
     @SuppressWarnings("unused")
     private boolean isInfinity(double realNumber) {
         // TODO: please find the method that perform this operation.
-        throw new NotImplementedException();
+        return new Double(realNumber).isInfinite();
     }
 }
